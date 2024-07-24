0ce3cc708f1794943847e3069ee976ac07b3b948
Pranjulflows <pranjuldheeman@gmail.com>
2024-07-24 18:12:08 +0530

    Swap function and bubble sort algorithm

---
+++ src/main/kotlin/BubbleSort.kt
@@ -0,0 +1,18 @@
+package org.macamps
+
+fun main() {
+    val unsorted = mutableListOf(9, 3, 4, 134, 45, 1, 4)
+
+    for (i in 0 until unsorted.size) {
+        for (j in 1 until unsorted.size - i) {
+            if (unsorted[j - 1] > unsorted[j]) {
+                val temp: Int = unsorted[j]
+                unsorted[j] = unsorted[j - 1]
+                unsorted[j - 1] = temp
+            }
+        }
+    }
+
+    print(unsorted)
+
+}
---
+++ src/main/kotlin/SwapFunction.kt
@@ -0,0 +1,15 @@
+package org.macamps
+
+fun main() {
+    var one = 20
+    var two = 30
+    var temp: Int
+
+    temp = one
+    one = two
+    two = temp
+
+    print("Swapped $one $two ")
+
+
+}
